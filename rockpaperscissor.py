a4b2babe3911bbedc63ae21a7151e7172f9e3c86
Dolapo <dolapo@dolapo@gmail.com>
2022-06-13 13:52:51 +0100

    little crrections made to code

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,31 +1,31 @@
-from random import randint
+from random import choice
 
-t = ["Rock", "Paper", "Scissors"]
+t = ["R", "P", "S"]
 
-computer = t[randint(0,2)]
+computer = choice(t)
 
 player = False
 
 while player == False:
-    player = input("Rock, Paper, Scissors: ")
+    player = input("R for Rock, P for Paper, S for Scissors: ")
     if player == computer:
         print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
+    elif player == "R":
+        if computer == "P":
+            print("You lose! paper covers rock")
         else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
+            print("You win! rock smashes paper")
+    elif player == "P":
+        if computer == "S":
+            print("You lose! scissors cut paper")
         else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
+            print("You win! paper covers scissors")
+    elif player == "S":
+        if computer == "R":
+            print("You lose... Rock smashes scissors")
         else:
-            print("You win!", player, "cut", computer)
+            print("You win! Scissors cut rock")
     else:
-        print("That's not a valid play. Check your spelling!")
+        print("That's not a valid play. Check your Input")
     player = False
-    computer = t[randint(0,2)]
+    computer = choice(t)
